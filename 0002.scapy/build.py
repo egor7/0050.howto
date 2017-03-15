dd2e43578ce415499564070bbce2f47775ec313e
Egor Shalashnikov <egor.shalashnikov@gmail.com>
2017-03-15 19:50:45 +0300

    3 fields done

--- 0002.scapy/build.py
+++ 0002.scapy/build.py
@@ -1,5 +1,17 @@
 from scapy.all import *
+conf.color_theme=NoTheme()
 
-a=IP(dst="ya.ru", id=0x42)
-print(repr(a))
-a.show()
+class P9(Packet):
+    name = "P9"
+    fields_desc=[LEIntField("size",0),
+                 ByteField("type",0),
+                 LEShortField("tag",0)]
+
+#p1=P9(size=11, type=1, tag=123)
+#hexdump(p1)
+
+bind_layers(TCP, P9, sport=5640)
+bind_layers(TCP, P9, dport=5640)
+p=rdpcap('5640.pcap')
+p[0][TCP].show()
+hexdump(p[0][P9])
--- 0002.scapy/build.sh
+++ 0002.scapy/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-python build.py 2>&1 | grep -ve "PyX" -e "IPv6" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | tee build.lst
+#python build.py 2>&1 | grep -ve "PyX" -e "IPv6" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | tee build.lst
+python build.py 2>&1 | grep -v -e "PyX" -e "IPv6" | tee build.lst
