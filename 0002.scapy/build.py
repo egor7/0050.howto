08a828af044de8fd7edec6042e03929c1554ca42
Egor Shalashnikov <egor.shalashnikov@gmail.com>
2017-04-15 22:18:38 +0300

    stat done

--- 0002.scapy/build.py
+++ 0002.scapy/build.py
@@ -1,5 +1,6 @@
 import struct
 from scapy.all import *
+from time import strftime,gmtime
 conf.color_theme=NoTheme()
 
 # === convert BYTE to STR
@@ -16,7 +17,7 @@ conf.color_theme=NoTheme()
 # print "{:0>8}".format(bin(int('0f', base=16))[2:])
 
 # === QID test
-# a = P9qid("qid", None)
+# a = P9qid("qid")
 # s = a.addfield(None, "", (P9qid.DIR | P9qid.TMP, 16, 32))
 # s = a.addfield(None, s, (P9qid.FILE | P9qid.TMP, 64, 128))
 # s,v1 = a.getfield(None, s)
@@ -29,6 +30,17 @@ conf.color_theme=NoTheme()
 #     fields_desc=[P9C("qid", None, P9C)]
 # p2 = P92000(qid=P9C(P9qid.FILE | P9qid.TMP, 16, 32))
 # print(repr(P9qid.fromstr(str(p2))))
+
+# # === P9stat test
+# a = P9stat("stat")
+# s = a.addfield(None, "", ((P9qid.DIR | P9qid.TMP, 16, 32),0,0,0,0,'t1','e1','s1','t1'))
+# s = a.addfield(None, s, ((P9qid.FILE | P9qid.TMP, 64, 128),0,0,0,0,'t2','e2','s2','t2'))
+# s,v1 = a.getfield(None, s)
+# s,v2 = a.getfield(None, s)
+# print(a.i2h(None, v1))
+# print(a.i2h(None, v2))
+# print(a.i2repr(None, v1))
+# print(a.i2repr(None, v2))
 
 # === Field access
 # p=rdpcap('5640-4.pcap')
@@ -70,8 +82,8 @@ p9types = { 100: "Tversion",  # size[4] Tversion tag[2]        msize[4] version[
             122: "Tremove",   # size[4] Tremove  tag[2] fid[4]
             123: "Rremove",   # size[4] Rremove  tag[2]
             124: "Tstat",     # size[4] Tstat    tag[2] fid[4]
-            125 : "Rstat",     # size[4] Rstat    tag[2]                 stat[n]
-            126 : "Twstat",    # size[4] Twstat   tag[2] fid[4]          stat[n]
+            125: "Rstat",     # size[4] Rstat    tag[2]                 stat[n]
+            126: "Twstat",    # size[4] Twstat   tag[2] fid[4]          stat[n]
             127: "Rwstat" }   # size[4] Rwstat   tag[2]
 
 # === param[n]: n-const
@@ -159,19 +171,19 @@ class P9Nnwqid(P9N):
 
 # === string[s]: s[2] + s bytes
 class P9S(Field):
-    def m2i(self, pkt, x):
-        return x[2:2+struct.unpack("<H", x[:2])[0]]
     def i2m(self, pkt, x):
         if x is None:
             x = ""
         elif type(x) is not str:
             x=str(x)
         return "" + struct.pack("<H", len(x)) + x
+    def m2i(self, pkt, x):
+        return x[2:2+struct.unpack("<H", x[:2])[0]]
+    def getfield(self, pkt, s):
+        x = self.m2i(pkt, s)
+        return s[2+len(x):],x
     def addfield(self, pkt, s, val):
         return s+self.i2m(pkt, val)
-    def getfield(self, pkt, s):
-        str = self.m2i(pkt, s)
-        return s[2+len(str):],str
 
 class P9Sversion(P9S):
     def __init__(self, default="9P2000"):
@@ -185,14 +197,9 @@ class P9Saname(P9S):
 class P9Sename(P9S):
     def __init__(self, default=""):
         P9S.__init__(self, "ename", default)
-class P9Sstat(P9S):
-    def __init__(self, default=""):
-        P9S.__init__(self, "stat", default)
 
 class P9qid(Field):
     """13-bytes qid"""
-    length  = 13
-
     DIR     = 0x80 # directories
     APPEND  = 0x40 # append only files
     EXCL    = 0x20 # exclusive use files
@@ -204,6 +211,9 @@ class P9qid(Field):
 
     def __init__(self, name):
         Field.__init__(self, name, None, "13s")
+    def i2len(self, pkt, x):
+        """Used in FieldLenField especially"""
+        return 13
     def i2m(self, pkt, x):
         """Convert internal(class) representation to machine(then packed by fmt) value"""
         (type, vers, path) = x
@@ -211,16 +221,15 @@ class P9qid(Field):
     def m2i(self, pkt, x):
         """Convert unpacked(fmt, from machine value) to internal(class) representation"""
         if x == '': return None
-        type = ord(x[0:1])
+        type = struct.unpack("<B", x[0:1])[0] # ord(x[0:1])
         vers = struct.unpack("<I", x[1:5])[0]
-        path = struct.unpack("<Q", x[5:])[0]
+        path = struct.unpack("<Q", x[5:13])[0]
         return (type, vers, path)
     def getfield(self, pkt, s):
-        l = self.length
-        return s[l:], self.m2i(pkt,s[:l])
+        x = self.m2i(pkt,s)
+        return s[self.i2len(pkt, x):], x
     def addfield(self, pkt, s, val):
-        l = self.length
-        return s+struct.pack("%is"%l,self.i2m(pkt, val))
+        return s+self.i2m(pkt, val)
     def i2h(self, pkt, x):
         return x
     def i2repr(self, pkt, x):
@@ -293,7 +302,6 @@ class P9Lwqid(P9L):
     def __init__(self, count_from):
         P9L.__init__(self, "wqid", P9qid(""), count_from=count_from)
 
-# TODO: like P9S - first 2 bytes contains size
 class P9stat(Field):
     """compound stat[n]"""
 
@@ -307,48 +315,97 @@ class P9stat(Field):
     # atime[4]:I     last access time
     # mtime[4]:I     last modification time
     # length[8]:Q    length of file in bytes
-    # name[ s ]:    file name; must be / if the file is the root directory of the server
-    # uid[ s ]:     owner name
-    # gid[ s ]:     group name
-    # muid[ s ]:    name of the user who last modified the file
+    # name[ s ]:     file name; must be / if the file is the root directory of the server
+    # uid[ s ]:      owner name
+    # gid[ s ]:      group name
+    # muid[ s ]:     name of the user who last modified the file
 
     def __init__(self, name):
         # stop bitching up with defaults, fmt, sz,
         # couse they are used only in get/addfield
         Field.__init__(self, name, None)
+    def i2len(self, pkt, x):
+        """Used in FieldLenField especially"""
+        ((qtype, qvers, qpath), mode, atime, mtime, length, name, uid, gid, muid) = x
+        return 41 + 2+len(name) + 2+len(uid) + 2+len(gid) + 2+len(muid)
     def i2m(self, pkt, x):
         """human-touple to byte-str"""
-        (size, type, dev, (qtype, qvers, qpath), mode, atime, mtime, length, name, uid, gid, muid) = x
-        s = struct.pack("<HHIBIQIIIQ", size, type, dev, qtype, qvers, qpath, mode, atime, mtime, length)
-        s += struct.pack("<H", name) + str(name)
-        s += struct.pack("<H", uid) + str(uid)
-        s += struct.pack("<H", qid) + str(qid)
-        s += struct.pack("<H", muid) + str(muid)
+        ((qtype, qvers, qpath), mode, atime, mtime, length, name, uid, gid, muid) = x
+        s  = struct.pack("<HHHI", self.i2len(pkt, x), self.i2len(pkt, x) - 2, 0, 0)
+        s += struct.pack("<BIQ", qtype, qvers, qpath)
+        s += struct.pack("<IIIQ", mode, atime, mtime, length)
+        s += struct.pack("<H", len(name)) + str(name)
+        s += struct.pack("<H", len(uid)) + str(uid)
+        s += struct.pack("<H", len(gid)) + str(gid)
+        s += struct.pack("<H", len(muid)) + str(muid)
         return s
     def m2i(self, pkt, x):
         """Convert byte-str to human-touple"""
         if x == '': return None
+        n = 0
+        ssize = struct.unpack("<H", x[n:n+2])[0]; n+=2
+        size = struct.unpack("<H", x[n:n+2])[0]; n+=2
+        type = struct.unpack("<H", x[n:n+2])[0]; n+=2
+        dev  = struct.unpack("<I", x[n:n+4])[0]; n+=4
+        qtype = struct.unpack("<B", x[n:n+1])[0]; n+=1
+        qvers = struct.unpack("<I", x[n:n+4])[0]; n+=4
+        qpath = struct.unpack("<Q", x[n:n+8])[0]; n+=8
+        #(qtype, qvers, qpath) = P9qid("").m2i(pkt, x[8:21])
+        mode = struct.unpack("<I", x[n:n+4])[0]; n+=4
+        atime = struct.unpack("<I", x[n:n+4])[0]; n+=4
+        mtime = struct.unpack("<I", x[n:n+4])[0]; n+=4
+        length = struct.unpack("<Q", x[n:n+8])[0]; n+=8
+        # name = P9S[41:43+struct.unpack("<H", x[41:43])[0]]
+        name = P9S("","").m2i(pkt, x[n:]); n+=2+len(name)
+        uid = P9S("","").m2i(pkt, x[n:]); n+=2+len(uid)
+        gid = P9S("","").m2i(pkt, x[n:]); n+=2+len(gid)
+        muid = P9S("","").m2i(pkt, x[n:]);
+        return ((qtype, qvers, qpath), mode, atime, mtime, length, name, uid, gid, muid)
+    def getfield(self, pkt, s):
+        x = self.m2i(pkt,s)
+        return s[2+self.i2len(pkt, x):], x
+    def addfield(self, pkt, s, val):
+        return s+self.i2m(pkt, val)
+    def i2h(self, pkt, x):
+        return x
+    def i2repr(self, pkt, x):
+        """Convert internal value to a nice representation"""
+        ((qtype, qvers, qpath), mode, atime, mtime, length, name, uid, gid, muid) = x
+        s = ""
+        #s += ":".join("{0:02x}".format(ord(c)) for c in self.i2m(pkt, x))
+        s += '('
+        if (P9qid.DIR     & qtype): s +=  'DIR'
+        else:                       s +=  'FILE'
+        if (P9qid.APPEND  & qtype): s += '|APPEND'
+        if (P9qid.EXCL    & qtype): s += '|EXCL'
+        if (P9qid.MOUNT   & qtype): s += '|MOUNT'
+        if (P9qid.AUTH    & qtype): s += '|AUTH'
+        if (P9qid.TMP     & qtype): s += '|TMP'
+        if (P9qid.SYMLINK & qtype): s += '|SYMLINK'
+        s += ',' + str(qvers)
+        s += ',' + str(qpath)
+        s += ') '
+        if (P9qid.DIR     & (mode & 0xFF000000)>>3*8): s += 'DIR-'
+        if (P9qid.APPEND  & (mode & 0xFF000000)>>3*8): s += 'APPEND-'
+        if (P9qid.EXCL    & (mode & 0xFF000000)>>3*8): s += 'EXCL-'
+        if (P9qid.MOUNT   & (mode & 0xFF000000)>>3*8): s += 'MOUNT-'
+        if (P9qid.AUTH    & (mode & 0xFF000000)>>3*8): s += 'AUTH-'
+        if (P9qid.TMP     & (mode & 0xFF000000)>>3*8): s += 'TMP-'
+        if (P9qid.SYMLINK & (mode & 0xFF000000)>>3*8): s += 'SYMLINK-'
+        s += str(oct(mode & 0x0000FFFF))
+        s += ' "' + name
+        s += '","' + uid
+        s += '","' + gid
+        s += '","' + muid
+        s += '" (' + strftime("%Y.%m.%d-%H:%M:%S", gmtime(mtime))
+        s += '),(' + strftime("%Y.%m.%d-%H:%M:%S", gmtime(atime))
+        s += ')'
+        return s
 
-        size = struct.unpack("<H", x[0:2])[0]
-        type = struct.unpack("<H", x[2:4])[0]
-        dev  = struct.unpack("<I", x[4:8])[0]
-        qtype = struct.unpack("<B", x[8:9])[0]
-        qvers = struct.unpack("<I", x[9:13])[0]
-        qpath = struct.unpack("<Q", x[13:21])[0]
-        mode = struct.unpack("<I", x[21:25])[0]
-        atime = struct.unpack("<I", x[25:29])[0]
-        mtime = struct.unpack("<I", x[29:33])[0]
-        length = struct.unpack("<Q", x[33:41])[0]
-        name[ s ]:    file name; must be / if the file is the root directory of the server
-        uid[ s ]:     owner name
-        gid[ s ]:     group name
-        muid[ s ]:    name of the user who last modified the file
+class P9Sstat(P9stat):
+    def __init__(self):
+        P9stat.__init__(self, "stat")
 
-
-        type = ord(x[0:1])
-        vers = struct.unpack("<I", x[1:5])[0]
-        path = struct.unpack("<Q", x[5:])[0]
-        return (type, vers, path)
 
 
 class P9(Packet):
@@ -386,9 +443,10 @@ class P9(Packet):
                 ]
     def mysummary(self):
         s = self.sprintf("%2s,P9.tag% %P9.type%")
-        #where is fid? [104,110,112,114,116,118,120,122,124,126]
         if self.type in [100,101]:
             s += self.sprintf(" %P9.version%")
+        if self.type in [104,110,112,114,116,118,120,122,124,126]:
+            s += self.sprintf(" %P9.fid%:fid")
         if self.type in [102,104]:
             s += self.sprintf(" %P9.uname%")
         if self.type in [107]:
@@ -402,11 +460,11 @@ class P9(Packet):
         if self.type in [113,115]:
             s += self.sprintf(" %P9.iounit%")
         if self.type in [110]:
-            s += self.sprintf(" %P9.newfid%")
-            s += self.sprintf(" %P9.nwname%")
+            s += self.sprintf(" %P9.newfid%:fidnew")
+            #s += self.sprintf(" %P9.nwname%")
             s += self.sprintf(" %P9.wname%")
         if self.type in [111]:
-            s += self.sprintf(" %P9.nwqid%")
+            #s += self.sprintf(" %P9.nwqid%")
             s += self.sprintf(" %P9.wqid%")
         if self.type in [125,126]:
             s += self.sprintf(" %P9.stat%")
@@ -418,5 +476,12 @@ bind_layers(TCP, P9, dport=5640)
 
 p=rdpcap('5640-4.pcap')
 p=p.filter(lambda x:x.haslayer(P9))[:]
-p.summary()
-##p[518][P9].show()
+p.nsummary()
+
+#hexdump(p[11][P9])
+#hexdump(p[518][P9])
+#p[11][P9].show()
+#p[518][P9].show()
+
+#hexdump(p[16][P9])
+#p[16][P9].show()
