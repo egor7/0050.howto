8741f7788624496306d76ca5f9f52683234bcf95
Egor Shalashnikov <egor.shalashnikov@gmail.com>
2017-04-19 20:39:55 +0300

    c9 in progress

--- 0003.c/build.sh
+++ 0003.c/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-tcc -run main.c | tee build.lst
-#python build.py 2>&1 | grep -v -e "PyX" -e "IPv6" | tee build.lst
+tcc c9.c -run main.c | tee build.lst
--- 0003.c/main.c
+++ 0003.c/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "c9.h"
 
 int main() {
-	printf("main/BEG\n");
+    printf("main/BEG\n");
 
     FILE *f;
     f = fopen("5640-1.9p", "rb");
@@ -11,15 +12,36 @@ int main() {
         return 1;
     }
 
-    char *tail;
+    long sum = 0;
     unsigned char ssize[4];
-    long size;
-    fread(ssize, 4, 1, f);
-    size = ssize[0] | (ssize[1]<<8) | (ssize[2]<<16) | (ssize[3]<<24);
-    printf("%d\n", size);
+    while (fread(ssize, 4, 1, f) > 0) {
+        long size;
+        size = ssize[0] | (ssize[1]<<8) | (ssize[2]<<16) | (ssize[3]<<24);
+        printf("%d\n", size);
+        sum += size;
+
+        size -= 4;
+        unsigned char *sbody = malloc(sizeof(unsigned char)*size);
+        fread(sbody, size, 1, f);
+
+        -- prepare internal C9t/C9r
+        c/s9proc()
+            C9ctx.read(size)
+            C9ctx.read(body)
+          C9t.t()/C9r.r() -- вешаем callback работать с приходящими сообщениями
+
+        c/s9_type_()
+            R/T()
+                C9ctx.begin()
+            C9ctx.end
+
+
+	free(sbody);
+    }
 
     fclose(f);
 
-	printf("main/END\n");
+    printf("main/sum = %d\n", sum);
+    printf("main/END\n");
     return 0;
 }
